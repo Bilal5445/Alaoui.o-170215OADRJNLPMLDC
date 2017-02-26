f9454d3b9ecd17ea6e4f7635e6704be3b0332df5
user01 <yahia.alaoui@gmail.com>
2017-02-26 10:29:33 +0000

    first commit

---
+++ ScrapyWeb/Business/Business.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScrapyWeb.Models;
+namespace ScrapyWeb.Business
+{
+    public class clBusiness
+    {
+        public static void getTwitterApplications(ref List<TwitterApplication> _appList)
+        {
+
+            using (var context = new ScrapyWeb.Models.ScrapyWebEntities())
+            {
+                // Load all blogs and related posts 
+                _appList  = context.TwitterApplications
+                                      .ToList();
+
+               
+
+            }
+ 
+        }
+    }
+}
