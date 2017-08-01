9415b4dd4bdf81525e3fe91b78e613c96c9e2b7e
namatedev <namatedev@localhost>
2017-08-01 15:47:20 +0100

    Ver 0.177.31.1 : cleaned for VM

---
+++ OADRJNLPCommon/OADRJNLPCommon/Business/PathConstant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OADRJNLPCommon.Business
+{
+    public class PathConstant
+    {
+        // public const String pathToArabiziEnv = @"C:\Users\Yahia Alaoui\Desktop\DEV\17028OADRJNLPARBZ\";
+        public const String pathToArabiziEnv = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f\";
+    }
+}
