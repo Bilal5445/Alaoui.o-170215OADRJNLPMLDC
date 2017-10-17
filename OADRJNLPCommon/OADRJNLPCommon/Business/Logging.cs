9e9e8ee8ab3aacef352891cfae429094883d4b61
namatedev <namatedev@localhost>
2017-10-17 10:19:36 +0000

    arabiziweb>ui>deadlock : added logging

---
+++ OADRJNLPCommon/OADRJNLPCommon/Business/Logging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OADRJNLPCommon.Business
+{
+    public class Logging
+    {
+        public static void Write(HttpServerUtilityBase Server, String message)
+        {
+            //
+            String pathdata = Server.MapPath("~/App_Data") + @"\log.txt";
+
+            // 04/04/14 17:51:48
+            message = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + " : " + message + "\r\n";
+
+            //
+            File.AppendAllText(pathdata, message);
+        }
+    }
+}
