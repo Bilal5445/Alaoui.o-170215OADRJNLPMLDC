899bcd7f9b7d20d0e71434803b13362522bd6645
namatedev <namatedev@localhost>
2017-10-17 11:18:36 +0000

    path (server/vm) now in web.config

--- OADRJNLPCommon/OADRJNLPCommon/Business/PathConstant.cs
+++ OADRJNLPCommon/OADRJNLPCommon/Business/PathConstant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,13 @@ namespace OADRJNLPCommon.Business
 {
     public class PathConstant
     {
+        public PathConstant()
+        {
+            pathToArabiziEnv = ConfigurationManager.AppSettings["pathToPerlScript"].ToString();
+        }
+
+        public String pathToArabiziEnv;
         // public const String pathToArabiziEnv = @"C:\Users\Yahia Alaoui\Desktop\DEV\17028OADRJNLPARBZ\";
-        public const String pathToArabiziEnv = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f\";
+        // public const String pathToArabiziEnv = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f\";
     }
 }
