5bd68fe3e1702a8380822646fb863e250249d6a5
namatedev <namatedev@localhost>
2017-11-24 12:43:49 +0000

    clean : minor check (for UT)

--- OADRJNLPCommon/OADRJNLPCommon/Business/Logging.cs
+++ OADRJNLPCommon/OADRJNLPCommon/Business/Logging.cs
@@ -12,14 +12,17 @@ namespace OADRJNLPCommon.Business
     {
         public static void Write(HttpServerUtilityBase Server, String message)
         {
-            //
-            String pathdata = Server.MapPath("~/App_Data") + @"\log.txt";
+            if (Server != null)
+            {
+                //
+                String pathdata = Server.MapPath("~/App_Data") + @"\log.txt";
 
-            // 04/04/14 17:51:48
-            message = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffffff") + " : " + System.Environment.MachineName + ": " + message + "\r\n";
+                // 04/04/14 17:51:48
+                message = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss.ffffff") + " : " + Environment.MachineName + ": " + message + "\r\n";
 
-            //
-            File.AppendAllText(pathdata, message);
+                //
+                File.AppendAllText(pathdata, message);
+            }
         }
     }
 }
