69b767e94819771b2c3e2e6b17699c94bba437e5
namatedev <namatedev@localhost>
2017-11-29 12:21:56 +0000

    scrappy push

--- OADRJNLPCommon/OADRJNLPCommon/Models/FB_POST.cs
+++ OADRJNLPCommon/OADRJNLPCommon/Models/FB_POST.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OADRJNLPCommon.Models
 {
+    [Table("T_FB_POST")]
     public partial class FB_POST
     {
+        [Key]
         public string id { get; set; }
         public string fk_influencer { get; set; }
         public string post_text { get; set; }
