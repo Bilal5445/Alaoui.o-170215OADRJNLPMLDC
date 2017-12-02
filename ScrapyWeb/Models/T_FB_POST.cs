43dd1da94ac8029d5b5823e2cbfc296f837e0d00
pc10 <pc10@localhost>
2017-12-02 18:42:13 +0530

    Add method for get the comments of posts

--- ScrapyWeb/Models/T_FB_POST.cs
+++ ScrapyWeb/Models/T_FB_POST.cs
@@ -19,6 +19,7 @@ namespace ScrapyWeb.Models
         public string id { get; set; }
         public string fk_influencer { get; set; }
         public string post_text { get; set; }
+        public string translated_text { get; set; }
         public int likes_count { get; set; }
         public int comments_count { get; set; }
         public System.DateTime date_publishing { get; set; }
