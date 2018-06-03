0ab04365490ec03278ad660a5494d236a81721e8
dev_vm_1 <dev_vm_1@localhost>
2018-06-03 20:57:37 +0000

    clean : .net 4.5.2 to 4.6.1

--- ScrapyWeb/Models/ScrapyWeb.Designer.cs
+++ ScrapyWeb/Models/ScrapyWeb.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Yahia Alaoui\Desktop\DEV\170215OADRJNLPMLDC\ScrapyWeb\Models\ScrapyWeb.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\user.DESKTOP-FK0DOCF\Desktop\DEV\170215OADRJNLPMLDC\ScrapyWeb\Models\ScrapyWeb.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
